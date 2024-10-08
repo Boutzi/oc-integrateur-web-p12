a19b9102858263ab56104ccd0c672c42ef488e4d
Boutzi <joe@arkaans.com>
2024-10-09 00:30:18 +0200

    feat: 404/500 errors

---
+++ app/error/page.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { Section } from "../_components/Section";
+import { Button } from "../_components/ui/button";
+
+const Error500 = () => {
+  return (
+    <Section className="flex justify-between gap-4 min-h-[calc(100vh-170px)] max-lg:pt-8 max-lg:pb-8">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
+            500
+          </h1>
+          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+            Internal Server Error.
+          </p>
+          <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+            We are already working to solve the problem.{" "}
+          </p>
+          <Link
+            href="/"
+            className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
+          >
+            <Button>Back to Homepage</Button>
+          </Link>
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Error500;
