a03da6c709e29927085384cbf0d29c3ea5eb7c18
Boutzi <joe@arkaans.com>
2024-10-10 18:57:31 +0200

    feat: footer versionning

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { GithubIcon } from "./icons/GithubIcon";
 import { JoeIcon } from "./icons/JoeIcon";
@@ -7,31 +7,36 @@ import { Section } from "./Section";
 import { ArrowBigUp } from "lucide-react";
 import { ScrollAnimation } from "./ScrollAnimation";
 import { usePathname } from "next/navigation";
+import VersionStatus from "./VersionStatus";
 
 export const Footer = () => {
-  const pathname = usePathname()
+  const pathname = usePathname();
   return (
     <footer className="relative">
-      {pathname === "/" && 
-      <ScrollAnimation
-        animated={true}
-        animationType="fade"
-        className="absolute right-4 bottom-4 group"
-        animationDelay={1000}
-      >
-        <Link href={"/"}>
-          <ArrowBigUp
-            size={48}
-            className="bg-accent/30 rounded-lg text-accent-foreground/70 animation-transform animate-pulse hover:bg-accent hover:text-accent-foreground hover:animate-none max-xl:hidden"
-          />
-        </Link>
-      </ScrollAnimation>}
+      {pathname === "/" && (
+        <ScrollAnimation
+          animated={true}
+          animationType="fade"
+          className="absolute right-4 bottom-4 group"
+          animationDelay={1000}
+        >
+          <Link href={"/"}>
+            <ArrowBigUp
+              size={48}
+              className="bg-accent/30 rounded-lg text-accent-foreground/70 animation-transform animate-pulse hover:bg-accent hover:text-accent-foreground hover:animate-none max-xl:hidden"
+            />
+          </Link>
+        </ScrollAnimation>
+      )}
       <Section className="flex border-t border-accent/70 w-full pt-4 pb-10 justify-between items-center">
         <div>
           <Link href={"/"}>
             <JoeIcon size={64} />
           </Link>
         </div>
+        <span className="text-xs text-accent-foreground/50">
+          version <VersionStatus />
+        </span>
         <div className="flex gap-4">
           <Link target="_blank" href={"https://github.com/Boutzi"}>
             <GithubIcon
