c6e025b1cd5d1201adbede67af3d2b1404cbd98c
Boutzi <joe@arkaans.com>
2024-10-11 22:25:43 +0200

    feat

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -17,7 +17,7 @@ export default function Error500() {
             We are already working to solve the problem.{" "}
           </p>
           <Link
-            href="/"
+            href="/test"
             className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
           >
             <Button>Back to Homepage</Button>
