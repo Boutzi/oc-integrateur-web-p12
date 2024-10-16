cdc5ab86949a6d327d8ca57a6c2bad3b317f2d05
Boutzi <joe@arkaans.com>
2024-10-17 00:02:24 +0200

    fix

--- app/[locale]/about/(me)/education/page.tsx
+++ app/[locale]/about/(me)/education/page.tsx
@@ -1,5 +1,14 @@
 import { EducationContainer } from "@/components/EducationContainer";
 
+export async function generateStaticParams() {
+  const locales = ["en", "fr", "kr"];
+  const paths = locales.map((locale) => ({
+    locale,
+  }));
+
+  return paths;
+}
+
 export default function Education() {
   return <EducationContainer />;
 }
