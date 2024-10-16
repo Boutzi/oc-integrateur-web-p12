d88136165e1e62d19baa21a172dd94542a09c6e3
Boutzi <joe@arkaans.com>
2024-10-16 23:53:42 +0200

    fix: params /experience/

--- app/[locale]/about/(me)/experience/page.tsx
+++ app/[locale]/about/(me)/experience/page.tsx
@@ -1,5 +1,21 @@
 import { ExperienceContainer } from "@/components/ExperienceContainer";
 
-export default function Experience() {
+// Définition des types pour les paramètres
+interface Params {
+  locale: string;
+}
+
+// Fonction pour générer les paramètres statiques
+export async function generateStaticParams() {
+  const locales = ["en", "fr", "kr"];
+  const paths = locales.map((locale) => ({
+    locale,
+  }));
+
+  return paths;
+}
+
+// Composant Experience
+export default function Experience({ params }: { params: Params }) {
   return <ExperienceContainer />;
 }
