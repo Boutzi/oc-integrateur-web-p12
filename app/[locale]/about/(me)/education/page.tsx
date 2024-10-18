3360b4f3988ebdd19354bc386cb1be81cddabd70
Boutzi <joe@arkaans.com>
2024-10-18 22:45:45 +0200

    try education

--- app/[locale]/about/(me)/education/page.tsx
+++ app/[locale]/about/(me)/education/page.tsx
@@ -1,9 +1,11 @@
 import { EducationContainer } from "@/components/EducationContainer";
-import { useLocale } from "next-intl";
 import { unstable_setRequestLocale } from "next-intl/server";
 
-export default function Education() {
-  const locale = useLocale();
+export default function Education({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
   unstable_setRequestLocale(locale);
   return <EducationContainer />;
 }
